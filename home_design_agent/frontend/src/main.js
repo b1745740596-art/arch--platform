e77af11f8ad631f8dad4e645d93263a61d3ffee2
b1745740596-art <b1745740596@gmail.com>
2026-08-19 12:17:31 +0800

    feat: add PWA install support

--- home_design_agent/frontend/src/main.js
+++ home_design_agent/frontend/src/main.js
@@ -17,3 +17,9 @@ app.use(router)
 app.use(i18n)
 app.use(ElementPlus)
 app.mount('#app')
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(() => {})
+  })
+}
